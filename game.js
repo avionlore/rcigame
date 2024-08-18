5d818e1045f838af4b262af750142f0ed15aa8d9
avionlore <coolblue.cst@gmx.de>
2024-08-18 09:03:55 +0200

    First version of buy zones

--- game.js
+++ game.js
@@ -1,10 +1,10 @@
 
 
-let zoneTypes = {
-    Residantial,
-    Commercial,
-    Industry,
-}
+const zoneTypes = Object.freeze({
+    Residantial: "Residantial",
+    Commercial: "Commercial",
+    Industry: "Industry",
+});
 
 let zones = {
     R: {
@@ -14,7 +14,7 @@ let zones = {
         costMultiplicator: 1.5,
         owned: 0,
     },
-}
+};
 
 
 let loadObject = {};
@@ -27,14 +27,14 @@ let gameObject = {
 let gameIds = {
     gameContainer: "gameContainer",
     creditValue: "creditValue",
-}
+};
 
 function setGameObject(key, value)
 {
     if(!(key in gameObject)) { return; }
     if(key in loadObject) { gameObject[key].value = loadObject[key].value; return;}
     gameObject[key].value = value;
-}
+};
 
 function mainScreen()
 {
@@ -42,7 +42,9 @@ function mainScreen()
 
 
     mainContainer.appendChild(creditContainer());
-}
+    mainContainer.appendChild(inventoryContainer());
+    mainContainer.appendChild(buyZonesContainer());
+};
 
 function creditContainer()
 {
@@ -59,19 +61,75 @@ function creditContainer()
 
     return container;
 
+};
+
+function inventoryContainer()
+{
+    const container = document.createElement("p");
+    for (const key in zones) {
+        const zone = zones[key];
+        const zoneContainer = document.createElement("p");
+        zoneContainer.textContent = `${zone.name}: ${zone.owned}`
+        container.appendChild(zoneContainer);
+    }
+
+    return container;
+}
+
+function buyZonesContainer()
+{
+    const container = document.createElement("p");
+    for (const key in zones) {
+        const zone = zones[key];
+        const buyZoneButton = document.createElement("button");
+        const zoneCost = getZoneCost(zone);
+        buyZoneButton.innerText = `Buy ${zone.name} (${zoneCost})`;
+        buyZoneButton.addEventListener('click', function(){
+            buyZone(zone, 1);
+        });
+        container.appendChild(buyZoneButton);
+    }
+
+    return container;
+}
+
+function getZoneCost(zone)
+{
+    let zoneCost = 0;
+    zoneCost = zone.costMultiplicator * zone.owned;
+    if(zoneCost<1) { zoneCost = 1};
+    return zoneCost;
+}
+
+function buyZone(zone, number)
+{
+    const zoneCost = getZoneCost(zone);
+    if(!canAfford(zoneCost, number))
+    {
+        return false;
+    }
+
+    zone.owned+=number;
+    gameObject.credits.value-= (zoneCost*number);
+}
+
+function canAfford(cost, number)
+{
+    if((cost * number) <= gameObject.credits.value) { return true};
+    return false;
 }
 
 function updateCredits()
 {
     setGameObject(gameObject.credits.name, gameObject.credits.value + gameObject.creditsBaseIncome.value);
     document.getElementById(gameIds.creditValue).innerHTML = gameObject.credits.value;
-}
+};
 
 function gameUpdate()
 {
     console.log("Tick");
     updateCredits();
-}
+};
 
 mainScreen();
 
